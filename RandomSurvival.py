734a9dfd0c1215c352130f88ee61078d7285b83d
Robyn <robynthecryptid66@gmail.com>
2020-06-17 16:41:15 +1000

    0.0.0.2
    - Added A massive chunk of code

--- RandomSurvival.py
+++ RandomSurvival.py
@@ -16,7 +16,9 @@ starting_amount = int(input(">>> "))
 for i in range(starting_amount):
     line(f"What is survivor {i}'s name?")
     name = input(">>> ")
-    survivor = [name, 0, 10, 15, ] #[name, age, energy, hunger]
+    survivor = [name, 0, 10, 15, random.randint(1, 3), random.randint(1,3)] 
+    #[name, age, energy, hunger, strength, intelligence]
+    line(survivor)
     survivors.append(survivor)
 tick = 0
 day = 0
@@ -25,4 +27,27 @@ while True:
     line(f"Day: {day}")
     while tick <= 24:
         tick += 1
-        for i in range(survivors): 
+        for i in range(survivors):
+            survivors[i][3] -= 1
+            if survivors[i][3] > 0:
+                print(f"{survivors[i][0]} Died of starvation")
+                del survivors[i]
+                i += 1
+            elif survivors[i][2] > 0:
+                survivors[i][2] -= 1
+                action = random.randint(0, 1)
+                if action == 0:
+                    print(f"{survivors[i][0]} slept")
+                    survivors[i][2] += random.randint(2, 4)
+                elif action == 1:
+                    action = random.randint(0, 1)
+                    if action == 0:
+                        print(f"{survivors[i][0]} went hunting")
+                        survivors[i][3] += random.randint(0, 3)
+                        survivors[i][4] += random.randint(0, 3)
+                    elif action == 1:
+                        print(f"{survivors[i][0]} went foraging")
+                        survivors[i][3] += random.randint(0, 3)
+                        survivors[i][5] += random.randint(0, 3)
+            else:
+                print(f"{survivors[i][0]} slept")
